25ad2bfc320abc8dc98c3ad88b86aba103e7ef44
Cihan Yılmaz <chnrzgr@gmail.com>
2024-12-28 18:49:47 +0100

    Initial Commit

---
+++ FindMe Watch App/FindMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMeApp.swift
+//  FindMe Watch App
+//
+//  Created by Cihan Yilmaz on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FindMe_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
